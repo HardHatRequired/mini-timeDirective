bc9a3aa065d62d894beb37c4165f02e2272fe5dd
HardHatArea <jon.hoopes@gmail.com>
2016-06-25 12:48:38 -0600

    fix push

---
+++ js/directive.js
@@ -0,0 +1,10 @@
+angular.module('timeApp').directive('showTime', function() {
+  return {
+    restrict: 'E',
+    templateUrl: '/views/time.html',
+
+    link: function(scope, elem, attr) {
+      
+    }
+  }
+})
